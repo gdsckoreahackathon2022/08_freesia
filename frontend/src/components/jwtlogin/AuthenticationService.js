1fe1cd1e361a15465f61ef6619bcb5cd1fbf2763
nrj022 <0602smj@gmail.com>
2022-02-05 01:29:22 +0900

    AuthenticationService modify

--- frontend/src/components/jwtlogin/AuthenticationService.js
+++ frontend/src/components/jwtlogin/AuthenticationService.js
@@ -50,11 +50,11 @@ class AuthenticationService {
     axios.interceptors.request.use(
       (config) => {
         //const token = localStorage.getItem("token");
-        if (token) {
-          console.log(token);
-          config.headers["Authorization"] = "Bearer " + token;
-          console.log(config.headers["Authorization"]);
-        }
+        //if (token) {
+        console.log(token);
+        config.headers["Authorization"] = "Bearer " + token;
+        console.log(config.headers["Authorization"]);
+        //}
         // config.headers['Content-Type'] = 'application/json';
         return config;
       },
