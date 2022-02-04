e0d777d807f0d288ec818197aeaf956e7355ebff
nrj022 <0602smj@gmail.com>
2022-02-04 22:59:19 +0900

    MyPage 조회

--- frontend/src/components/mypage/Mypage.js
+++ frontend/src/components/mypage/Mypage.js
@@ -1,17 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import NavBar from "../navbar/Navbar";
 import styles from "./Mypage.module.css";
 
 function MyPage() {
-  const [nickname, setNickname] = useState("nickname");
+  const [nickname, setNickname] = useState("");
 
-  /* const userid = localStorage.getItem("authenticatedUser");
-  //const baseUrl =
+  const userid = localStorage.getItem("authenticatedUser");
+  const baseUrl = "http://localhost:8080";
 
   const getData = async () => {
-    const response = await axios.get(baseUrl+'/mypage?userid='+userid)
-    //setNickname(response.data.)
-  } */
+    const response = await axios.get(baseUrl + "/user?userid=" + userid);
+    setNickname(response.data.nickname);
+    console.log(response.data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <main>
       <NavBar />
