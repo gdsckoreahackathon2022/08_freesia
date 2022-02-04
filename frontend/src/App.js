e36a641343b34f28968505c0c7a3ead0c0535639
KIM MINJI <sml07113@gmail.com>
2022-02-05 00:23:33 +0900

    [feat] 로그인 토큰 없으면 로그인 페이지로 리다이렉트

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import Login from "./components/jwtlogin/Login";
 import Signup from "./components/jwtlogin/Signup";
@@ -10,13 +10,28 @@ import Edit from "./components/challenge/Edit";
 import Mypage from "./components/mypage/Mypage";
 
 function App() {
+
+  // 토큰 없으면 로그인 페이지로 리다이렉트
+  const token = localStorage.getItem("token");
+  function RequireAuth({ children }) {
+    let location = useLocation();
+    if (!token) {
+      return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+    return children;
+  }
+
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
+          <Route path="/" element={
+            <RequireAuth>
+              <Home />
+            </RequireAuth>
+          } />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/" element={<Home />} />
           <Route path="/challenge" element={<List />} />
           <Route path="/create" element={<Create />} />
           <Route path="/post/:id" element={<Detail />} />
